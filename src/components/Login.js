13e5185a8ef384a2627f25bfee1079a37ccaf4e5
Anant <ananttomer.4@gmail.com>
2023-10-24 11:20:50 +0530

    With Login page

---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/a73c4363-1dcd-4719-b3b1-3725418fd91d/fe1147dd-78be-44aa-a0e5-2d2994305a13/IN-en-20231016-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt=""
+        />
+      </div>
+      <form></form>
+    </div>
+  );
+};
+
+export default Login;
