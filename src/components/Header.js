77e5529b4c378eab40482488d753a7a75bb086da
Anant <ananttomer.4@gmail.com>
2023-10-28 20:06:01 +0530

    ProfileIcon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,51 @@
 import React from "react";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
+  const [isHovering, setHovering] = useState(false);
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
+
   return (
-    <div className="absolute px-20 py-2 bg-gradient-to-b from-black z-10">
+    <div className="absolute w-full px-20 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-40"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
+      {props.showProfileIcon && (
+        <div
+          className="flex cursor-pointer bottom-5 border-black px-3 py-5"
+          onMouseEnter={() => setHovering(true)}
+          onMouseLeave={() => setHovering(false)}
+        >
+          <img
+            className="w-10 h-10"
+            src="https://occ-0-2164-2186.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABVbYEW3Bkl6g7KkyJmusw_jPAXtpY6KusGOVNs-4v4vTLf6h4kETNq3ApIhFAHL2Dbe1z1T-j8gaEIw5TElx87IQfk791_g.png?r=5eb"
+            alt="logo"
+          />
+          {isHovering && (
+            <div className="absolute min-w-[100px] shadow-sm z-[1] text-white font-bold bg-gray-400 top-[80%]">
+              <button className="p-2 m-1" onClick={handleSignOut}>
+                Sign Out
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -71,7 +71,7 @@ const Login = () => {
 
   return (
     <div>
-      <Header />
+      <Header showProfileIcon={false}/>
       <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/a73c4363-1dcd-4719-b3b1-3725418fd91d/fe1147dd-78be-44aa-a0e5-2d2994305a13/IN-en-20231016-popsignuptwoweeks-perspective_alpha_website_large.jpg"
