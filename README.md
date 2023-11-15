b28efe937b3d225799d70bd742ca8db9547b4723
Anant <ananttomer.4@gmail.com>
2023-11-16 02:34:46 +0530

    Arrows for horizontal scroll on movies lists.

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # Netflix clone with ChatGPT powers
 
-Web-app hosting url: https://cinewave-demo.web.app/
+# Web-app hosting url: https://cinewave-demo.web.app/
 
-Steps to login:
+# Tech Stack
+    - React.js ; Redux; Tailwind CSS; JSX; 
+    - TMDb API for movie data
+    - Authentication with Firebase
+    - Deployment with Firebase
+
+# Steps to login:
 --Sign up to the application with your Email Id and password.
 --Sign in by clicking on "Already Registered? Sign In now".
+
+# Features:
+
+-- Login Page
+    - Validation of Mail & Password
+    - Authentication of user with Firebase
+    - Sign up and user resgistration by email
+
+-- Header
+    - Logo
+    - User Profile (when login is successsful)
+    - Sign Out on hover on the profile picture
+
+-- Browse Movie Page
+    - Video Backgraound for trailers playing with title/description appearing on the screen
+    - Movie Carousel with different movie lists
+    - Buttons to scroll rigth and left [on hover]
+    -
+
+-- Footer   
+    - Developer's contact information
+    - Contains redireting links to GitHub, Twitter, Linkedin, Medium profiles.
+
+
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
-  if (!movies) return;
+  const [Hovering, setHovering] = useState(false);
+  const scrollContainerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft -= 250;
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft += 250;
+    }
+  };
 
   return (
     <div className="px-6">
       <h1 className="text-2xl font-bold text-white p-2">{title}</h1>
-      <div className="flex overflow-x-auto no-scrollbar">
+      <div
+        className="flex overflow-x-auto no-scrollbar"
+        ref={scrollContainerRef}
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+      >
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
           ))}
         </div>
+        {Hovering && (
+          <div>
+            (
+            <button
+              className="absolute mt-[7%]  transform -translate-y-1/2 bg-gray-500 text-white p-3 rounded-full left-5"
+              onClick={scrollLeft}
+            >
+              ⫷
+            </button>
+            <button
+              className="absolute mt-[5%] transform -translate-y-1/2 bg-gray-500 text-white p-3 rounded-full right-5"
+              onClick={scrollRight}
+            >
+              ⫸
+            </button>
+            )
+          </div>
+        )}
       </div>
     </div>
   );
